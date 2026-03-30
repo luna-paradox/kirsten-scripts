30b29eb0fae6086289419e932e77ff7611a22f9f
luna-paradox <luna_paradox@proton.me>
2026-03-30 00:08:26 -0300

    add cheat shortcut

--- kittens-scripts/main.js
+++ kittens-scripts/main.js
@@ -1,15 +1,14 @@
 console.log('> Loading main.js script')
+var debug = false
 
-let debug = false
-
+//$ ---- COOKIE MANAGEMENT ----
 // Workshop Marks
-let COOKIE_KEY_IMPORTANT_MARKS = 'upgrade-important-marks'
-let COOKIE_KEY_MID_MARKS = 'upgrade-mid-marks'
-let COOKIE_KEY_NON_IMPORTANT_MARKS = 'upgrade-non-important-marks'
+var COOKIE_KEY_IMPORTANT_MARKS = 'upgrade-important-marks'
+var COOKIE_KEY_MID_MARKS = 'upgrade-mid-marks'
+var COOKIE_KEY_NON_IMPORTANT_MARKS = 'upgrade-non-important-marks'
 // Automation
-let COOKIE_KEY_AUTOMATION_FLAGS = 'automation-flags'
+var COOKIE_KEY_AUTOMATION_FLAGS = 'automation-flags'
 
-//* COOKIE MANAGEMENT
 var set_cookie = (name, value, days) => {
     var expires = '';
     if (days) {
@@ -32,9 +31,8 @@ var get_cookie = (name) => {
 }
 
 
-
-//* GAME QUERIES
-let get_res_percentage = (resource_id) => {
+//$ ---- GAME QUERIES ----
+var get_res_percentage = (resource_id) => {
     let resource = game.resPool.get(resource_id)
     let current = resource.value
     let max = resource.maxValue
@@ -43,7 +41,25 @@ let get_res_percentage = (resource_id) => {
     return percentage;
 }
 
-let craft_amount = (resource_id, amount_to_craft, log = true) => {
+var craft_amount = (resource_id, amount_to_craft, log = true) => {
     if (log || debug) console.log(`Crafting [${resource_id}] amount of [${amount_to_craft}]`)
     game.craft(resource_id, amount_to_craft)
 }
+
+var cheat_amount = (resource_id, amount) => {
+    game.resPool.addResEvent(resource_id, amount)
+}
+
+
+//$ ---- CUSTOM KEYBOARD SHORTCUTS ----
+window.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.code === 'Space') {
+        e.preventDefault(); 
+        
+        console.log("Cheat Shortcut triggered");
+        game.resPool.addResEvent('wood', 10000000)
+        game.resPool.addResEvent('minerals', 10000000)
+        game.resPool.addResEvent('iron', 10000000)
+        game.resPool.addResEvent('science', 10000000)
+    }
+}, false);
